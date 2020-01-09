42a84c0b199ebb6ab9b1b483a54fb411b0e94498
silence <silence@aliyun.com>
2020-01-09 13:24:09 +0800

    init spring boot

---
+++ plink-web/src/main/java/com/github/hairless/plink/web/BootStrapApp.java
@@ -0,0 +1,12 @@
+package com.github.hairless.plink.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrapApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStrapApp.class, args);
+    }
+}
