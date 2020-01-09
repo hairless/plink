478868314a35069cec930fa31dd7e81ab22c299e
sllence <762246344@qq.com>
2020-01-09 22:32:22 +0800

    Front and back integration (#7)

--- plink-web/src/main/java/com/github/hairless/plink/web/controller/IndexController.java
+++ plink-web/src/main/java/com/github/hairless/plink/web/controller/IndexController.java
@@ -1,14 +1,14 @@
 package com.github.hairless.plink.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = {"/", "/page/**"})
     public String index() {
-        return "hello plink";
+        return "index.html";
     }
 
 }
