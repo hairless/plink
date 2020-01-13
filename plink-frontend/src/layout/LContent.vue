49c367c41d13efe58f1f5fb61a794d9772089e16
wangyangting <yting_gmin0510@163.com>
2020-01-13 19:10:03 +0800

    Layout Items

---
+++ plink-frontend/src/layout/LContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <Content :style="{ padding: '12px', minHeight: '768px', background: '#fff' }">
+    <router-view />
+  </Content>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class LContent extends Vue {}
+</script>
+
