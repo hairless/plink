d40ca42163a73b6f7cfc0c40c14b37f30ae20703
sllence <762246344@qq.com>
2020-01-13 22:11:10 +0800

    job and jobInstance model (#11)

---
+++ plink-model/src/main/java/com/github/hairless/plink/model/pojo/Job.java
@@ -0,0 +1,63 @@
+package com.github.hairless.plink.model.pojo;
+
+import com.github.hairless.plink.model.enums.InstanceStatusEnum;
+import com.github.hairless.plink.model.enums.JobTypeEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * Flink 作业
+ *
+ * @author silence
+ * @date 2020/01/13
+ */
+@Getter
+@Setter
+@Builder
+public class Job extends BaseModel {
+    /**
+     * Flink作业名称，全局唯一
+     */
+    private String name;
+    /**
+     * 作业描述
+     */
+    private String description;
+    /**
+     * 作业类型
+     * {@link JobTypeEnum}
+     */
+    private Integer type;
+    /**
+     * Flink 客户端版本
+     */
+    private String clientVersion;
+    /**
+     * 作业配置
+     */
+    private String config;
+    /**
+     * 最新实例状态
+     * {@link JobInstance#status}
+     * {@link InstanceStatusEnum}
+     */
+    private Integer last_status;
+    /**
+     * 最新实例对应的集群任务id
+     * {@link JobInstance#appId}
+     */
+    private String lastAppId;
+    /**
+     * 最新实例启动时间
+     * {@link JobInstance#startTime}
+     */
+    private Date lastStartTime;
+    /**
+     * 最新实例停止时间
+     * {@link JobInstance#stopTime}
+     */
+    private Date lastStopTime;
+}
---
+++ plink-model/src/main/java/com/github/hairless/plink/model/pojo/JobInstance.java
@@ -0,0 +1,47 @@
+package com.github.hairless.plink.model.pojo;
+
+import com.github.hairless.plink.model.enums.InstanceStatusEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * Flink 作业实例
+ *
+ * @author silence
+ * @date 2020/01/13
+ */
+@Getter
+@Setter
+@Builder
+public class JobInstance extends BaseModel {
+    /**
+     * 作业id
+     * {@link Job#id}
+     */
+    private Long jobId;
+    /**
+     * 实例启动时的镜像作业配置
+     * {@link Job#config}
+     */
+    private String config;
+    /**
+     * 状态
+     * {@link InstanceStatusEnum}
+     */
+    private Integer status;
+    /**
+     * 提交到集群返回的任务id
+     */
+    private String appId;
+    /**
+     * 启动时间
+     */
+    private Date startTime;
+    /**
+     * 停止时间
+     */
+    private Date stopTime;
+}
