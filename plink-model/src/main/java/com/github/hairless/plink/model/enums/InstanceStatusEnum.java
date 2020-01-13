99ef57842f73167f9ad9a5bc34cf7309449fb78e
sllence <762246344@qq.com>
2020-01-13 22:11:10 +0800

    job and jobInstance model (#11)

---
+++ plink-model/src/main/java/com/github/hairless/plink/model/enums/InstanceStatusEnum.java
@@ -0,0 +1,30 @@
+package com.github.hairless.plink.model.enums;
+
+import lombok.Getter;
+
+/**
+ * 作业实例状态枚举
+ *
+ * @author silence
+ * @date 2020/01/13
+ */
+@Getter
+public enum InstanceStatusEnum {
+    NEW(0, "新建"),
+    STARTING(1, "启动中"),
+    RUNNING(2, "运行中"),
+    START_FAILED(3, "启动失败"),
+    RUN_FAILED(4, "运行失败"),
+    STOPPED(5, "已停止"),
+    SUCCESS(6, "运行成功"),
+    UNKNOWN(-1, "未知");
+
+    private Integer value;
+    private String desc;
+
+    InstanceStatusEnum(Integer value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+}
--- plink-model/src/main/java/com/github/hairless/plink/model/pojo/BaseModel.java
+++ plink-model/src/main/java/com/github/hairless/plink/model/pojo/BaseModel.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * Created by silence on 2020/01/10.
@@ -11,7 +12,18 @@ import javax.persistence.Id;
 @Getter
 @Setter
 public class BaseModel {
+    /**
+     * 主键自增id
+     */
     @Id
     private Long id;
+    /**
+     * 记录创建时间
+     */
+    private Date createTime;
+    /**
+     * 记录修改时间
+     */
+    private Date updateTime;
 
 }
