6671496af5cfe54c0b81883091fbff342d88d758
sllence <762246344@qq.com>
2020-01-15 21:07:00 +0800

    improve job api (#25)

--- plink-model/src/main/java/com/github/hairless/plink/model/pojo/JobInstance.java
+++ plink-model/src/main/java/com/github/hairless/plink/model/pojo/JobInstance.java
@@ -3,6 +3,7 @@ package com.github.hairless.plink.model.pojo;
 import com.github.hairless.plink.model.enums.JobInstanceStatusEnum;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
@@ -16,6 +17,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 public class JobInstance extends BaseModel {
     private static final long serialVersionUID = 1L;
     /**
