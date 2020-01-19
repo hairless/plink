406dedcd492558f54fb4beb0e1a247bdda92efda
backbook <30772565+backbook@users.noreply.github.com>
2020-01-19 12:18:10 +0800

    实现plink启动standalone方式代码 (#32)
    * 初步实现plink的stanalone模式启动jar
    
    * 初步实现plink的stanalone模式启动jar

--- plink-web/src/test/java/com/github/hairless/plink/DemoTest.java
+++ plink-web/src/test/java/com/github/hairless/plink/DemoTest.java
@@ -2,10 +2,15 @@ package com.github.hairless.plink;
 
 import com.github.hairless.plink.dao.mapper.PlinkTestMapper;
 import com.github.hairless.plink.model.pojo.PlinkTest;
+import com.github.hairless.plink.model.resp.Result;
+import com.github.hairless.plink.model.resp.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,5 +27,27 @@ public class DemoTest extends BaseTest {
     public void demoTest() {
         List<PlinkTest> plinkTestList = plinkTestMapper.selectAll();
         log.info(plinkTestList.toString());
+    }
+
+    @Test
+    public void upload(){
+        String jobId  = "2321";
+        String parentDir = System.getProperty("user.dir");
+        try {
+            File uploadPath = new File(parentDir + "/uploadJars/" + jobId);
+            if (uploadPath.exists()) {
+                String[] fileNames = uploadPath.list();
+                log.error(parentDir);
+
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String parentDir = System.getProperty("user.dir");
+        System.out.println(parentDir);
     }
 }
