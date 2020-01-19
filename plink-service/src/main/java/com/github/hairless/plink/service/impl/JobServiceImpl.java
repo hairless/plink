afe9ec89070010c67a956143181a7c8c6b4611e6
backbook <30772565+backbook@users.noreply.github.com>
2020-01-19 12:18:10 +0800

    实现plink启动standalone方式代码 (#32)
    * 初步实现plink的stanalone模式启动jar
    
    * 初步实现plink的stanalone模式启动jar

--- plink-service/src/main/java/com/github/hairless/plink/service/impl/JobServiceImpl.java
+++ plink-service/src/main/java/com/github/hairless/plink/service/impl/JobServiceImpl.java
@@ -1,6 +1,9 @@
 package com.github.hairless.plink.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.arronlong.httpclientutil.exception.HttpProcessException;
+import com.github.hairless.plink.common.HttpUtil;
 import com.github.hairless.plink.common.PageInfoUtil;
 import com.github.hairless.plink.dao.mapper.JobMapper;
 import com.github.hairless.plink.model.pojo.Job;
@@ -13,6 +16,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,6 +37,9 @@ import java.util.List;
 public class JobServiceImpl implements JobService {
     @Autowired
     private JobMapper jobMapper;
+    //注入系统环境，拿到yml文件
+    @Autowired
+    private Environment env;
 
     @Override
     public Result<JobResp> addJob(JobReq jobReq) {
@@ -168,6 +175,47 @@ public class JobServiceImpl implements JobService {
             log.warn("get jar list fail! jobId={}", jobId, e);
             return new Result<>(ResultCode.EXCEPTION, e);
         }
+    }
+
+
+
+    @Override
+    public Result startJob(JSONObject jsonObject) {
+
+        //flinkURL 例如：localhost:8081
+        String flinkURL = "http://"+env.getProperty("flink.restful.ip")+":"+env.getProperty("flink.restful.port");
+        //本地jar包存放路径
+        String parentDir = System.getProperty("user.dir");
+        String jarPath = parentDir + "/uploadJars/"+jsonObject.getString("jarName");
+
+        //向flink平台提交jar
+        String resJson = null;
+        try {
+            resJson = HttpUtil.sendFlinkJar(flinkURL, jarPath);
+        } catch (HttpProcessException e) {
+            return new Result<>(ResultCode.EXCEPTION, e);
+        }
+        JSONObject flinkRestRes = JSON.parseObject(resJson);
+        if ( !flinkRestRes.getString("status").equals("success")) {
+            return new Result(ResultCode.EXCEPTION,flinkRestRes.getString("status"));
+        }
+        String filename = flinkRestRes.getString("filename");
+        String filenames [] = filename.split("/");
+        //预留向mysql写表 id
+        String id = filenames[filenames.length-1];
+
+        String startHttpURL= flinkURL + "/v1/jars/"+id+"/run";
+
+        jsonObject.remove("jarName");
+        String senJobJsonRes = HttpUtil.sendPost(startHttpURL, jsonObject.toJSONString());
+
+        JSONObject jsonRes = JSON.parseObject(senJobJsonRes);
+        //预留向mysql写表 jobid
+        String jobid = (String) jsonRes.getOrDefault("jobid","FAIL");
+        if (jobid.equals("FAIL")){
+            return new Result(ResultCode.FAILURE,jobid);
+        }
+        return new Result(ResultCode.SUCCESS,jobid);
     }
 
 }
