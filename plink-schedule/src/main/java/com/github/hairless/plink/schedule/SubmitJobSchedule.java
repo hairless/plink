7e9527ec8563544cb9a5f79db680df97a75e0deb
sllence <762246344@qq.com>
2020-01-27 18:18:08 +0800

    schedule submit job and sync status (#44)

---
+++ plink-schedule/src/main/java/com/github/hairless/plink/schedule/SubmitJobSchedule.java
@@ -0,0 +1,38 @@
+package com.github.hairless.plink.schedule;
+
+import com.github.hairless.plink.dao.mapper.JobInstanceMapper;
+import com.github.hairless.plink.model.enums.JobInstanceStatusEnum;
+import com.github.hairless.plink.model.pojo.JobInstance;
+import com.github.hairless.plink.schedule.task.SubmitJobTask;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 作业异步提交定时任务
+ * 扫描待启动的实例进行异步的提交
+ * @author: silence
+ * @date: 2020/1/27
+ */
+@Slf4j
+@Component
+public class SubmitJobSchedule {
+    @Autowired
+    private JobInstanceMapper jobInstanceMapper;
+    @Autowired
+    private SubmitJobTask submitJobTask;
+
+    @Scheduled(fixedDelay = 5 * 1000, initialDelay = 10 * 1000)
+    public void submitJobSchedule() throws Exception {
+        log.info("submitJobSchedule start");
+        JobInstance condition = new JobInstance();
+        condition.setStatus(JobInstanceStatusEnum.WAITING_START.getValue());
+        List<JobInstance> jobInstances = jobInstanceMapper.select(condition);
+        for (JobInstance jobInstance : jobInstances) {
+            submitJobTask.asyncSubmitJobTask(jobInstance);
+        }
+    }
+}
--- plink-web/src/main/java/com/github/hairless/plink/web/BootStrapApp.java
+++ plink-web/src/main/java/com/github/hairless/plink/web/BootStrapApp.java
@@ -2,9 +2,11 @@ package com.github.hairless.plink.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableScheduling
 @EnableTransactionManagement
 @MapperScan("com.github.hairless.plink.dao.mapper")
 @SpringBootApplication(scanBasePackages = {"com.github.hairless.plink"})
