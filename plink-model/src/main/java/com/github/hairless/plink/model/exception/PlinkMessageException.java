84eecd4039f1a2137e8d57ddaf505bc294f7c18a
sllence <762246344@qq.com>
2020-02-17 22:03:17 +0800

    improve flink config and Exception (#52)
    * format
    
    * improve flink config and Exception

---
+++ plink-model/src/main/java/com/github/hairless/plink/model/exception/PlinkMessageException.java
@@ -0,0 +1,13 @@
+package com.github.hairless.plink.model.exception;
+
+/**
+ * @author: silence
+ * @date: 2020/2/17
+ */
+public class PlinkMessageException extends PlinkException {
+
+    public PlinkMessageException(String message) {
+        super(message);
+    }
+
+}
