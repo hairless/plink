d1e152a05ee4b7f3fd917858aba935013b11f697
sllence <762246344@qq.com>
2020-02-17 19:49:18 +0800

    improve upload jar path (#51)
     improve upload jar path

---
+++ plink-common/src/main/java/com/github/hairless/plink/common/UploadUtil.java
@@ -0,0 +1,16 @@
+package com.github.hairless.plink.common;
+
+/**
+ * @author: silence
+ * @date: 2020/2/17
+ */
+public class UploadUtil {
+    public static String getJobJarsPath(){
+        return getUploadBasePath()+"jobJars/";
+    }
+
+    public static String getUploadBasePath(){
+        String userDir = System.getProperty("user.dir");
+        return userDir + "/upload/";
+    }
+}
