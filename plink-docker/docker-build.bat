93a2a17efb68c0664f611c7c68a73d7bcb3ccb3a
wangyangting <yting_gmin0510@163.com>
2020-07-01 09:27:14 +0800

    release tag 0.1.0

--- plink-docker/docker-build.bat
+++ plink-docker/docker-build.bat
@@ -3,6 +3,6 @@ cd .. & ^
 mvn clean package -Dmaven.test.skip=true & ^
 copy /Y plink-web\target\*.jar plink-docker & ^
 copy /Y plink-web\src\main\resources\META-INF\sql\mysql\plink_init.sql plink-docker & ^
-docker build -t hairless/plink:master plink-docker & ^
+docker build -t hairless/plink:0.1.0 plink-docker & ^
 cd plink-docker & ^
 echo "--------- build success ---------"
