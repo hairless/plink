d3efd5242b577ddd83bb86ccee3d9d3e9acc5dec
soap <yangfuzhao@aliyun.com>
2020-09-04 15:27:08 +0800

    重写toString

--- plink-model/src/main/java/com/github/hairless/plink/model/pojo/BaseModel.java
+++ plink-model/src/main/java/com/github/hairless/plink/model/pojo/BaseModel.java
@@ -1,5 +1,6 @@
 package com.github.hairless.plink.model.pojo;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -39,7 +40,7 @@ public class BaseModel implements Serializable {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        return JSON.toJSONString(this);
     }
 
 }
