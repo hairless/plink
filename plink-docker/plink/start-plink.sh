207ddaf50a3a758809aa3ad0f0107ea8aaf69c73
John <lizhy2@corp.21cn.com>
2021-02-10 00:13:31 +0800

    Feat(new docker-build.bat): Add new build way.
    代码：
    1. 添加新的docker 构建方式(无需任何干预自动build)。
    
    解决问题：
    1. 解决docker 构建落后于版本，无法使用的问题
    2. 拆分mysql 与 plink 镜像(plink 使用 centos，默认自带vim 等工具，方便使用)
    3. 使用清华源加速依赖下载(更新版本需要手动更改源；国内源会过期)
    4. 使用 openjdk 代替 oracle jdk

---
+++ plink-docker/plink/start-plink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# flink cluster
+setsid /opt/flink/bin/start-cluster.sh >/var/log/flink-cluster.log 2>&1
+## mysql privileges
+## plink
+sed -i 's/localhost:3306/mysql-docker:3306/g' /opt/plink/config/application-prod.yml
+sed -i 's/localhost:3306/mysql-docker:3306/g' /opt/plink/config/application-local.yml
+$PLINK_HOME/bin/run.sh start
