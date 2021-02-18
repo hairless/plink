06ae42cdd9d2c9b7f77ae133ce0ef6d921a9e9af
silence <silence@aliyun.com>
2021-02-18 20:22:39 +0800

    test

--- plink-common/src/main/java/com/github/hairless/plink/common/conf/FlinkCommonSubmitOptions.java
+++ plink-common/src/main/java/com/github/hairless/plink/common/conf/FlinkCommonSubmitOptions.java
@@ -2,6 +2,7 @@ package com.github.hairless.plink.common.conf;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.github.hairless.plink.model.common.UIOption;
+import com.google.common.collect.Lists;
 import lombok.Getter;
 import org.apache.flink.runtime.jobgraph.tasks.CheckpointCoordinatorConfiguration;
 import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
@@ -21,18 +22,18 @@ public enum FlinkCommonSubmitOptions {
                     .desc("checkPoint间隔，单位毫秒")
                     .validator(c -> Preconditions.checkState(
                             Long.parseLong(c) < CheckpointCoordinatorConfiguration.MINIMAL_CHECKPOINT_TIME,
-                            "checkPoint间隔应该大于10ms")));
+                            "checkPoint间隔应该大于10ms"))),
 
-/*    CHECKPOINTING_MODE(ExecutionCheckpointingOptions.CHECKPOINTING_MODE.key(),
+    CHECKPOINTING_MODE(ExecutionCheckpointingOptions.CHECKPOINTING_MODE.key(),
             UIOption.RADIO.name("cp模式").desc("checkPoint模式，exactly-once or at-least-once")
                     .options(Lists.newArrayList("exactly-once", "at-least-once"))
                     .defaultValue("exactly-once")
                     .validator(c -> Preconditions.checkState(
-                            this.uiOption.getOptions().contains(c),
-                            "checkPoint间隔应该大于10ms")));*/
+                            Lists.newArrayList("exactly-once", "at-least-once").contains(c),
+                            "checkPoint模式应该为exactly-once或at-least-once")));
 
-    private String key;
-    private UIOption uiOption;
+    private final String key;
+    private final UIOption uiOption;
 
     FlinkCommonSubmitOptions(String key, UIOption uiOption) {
         this.key = key;
