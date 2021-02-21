8a2bfaa86fa253b235a92e498b1f006147bd3e59
John <lizhy2@corp.21cn.com>
2021-02-21 10:55:46 +0800

    Feat(new docker-build.bat): Add new build way.
    代码：
    1. 添加新的docker 构建方式(无需任何干预自动build)。
    
    解决问题：
    1. 解决docker 构建落后于版本，无法使用的问题
    2. 拆分mysql 与 plink 镜像(plink 使用 centos，默认自带vim 等工具，方便使用)
    3. 使用清华源加速依赖下载&&使用 openjdk 代替 oracle jdk
    4. 修复sql 初始化文件必须使用\n 作为分割符号，\r\n(windows 分割符) 会导致Docker build 失败

--- plink-docker/docker-build.bat
+++ plink-docker/docker-build.bat
@@ -2,13 +2,10 @@ echo "--------- build start ---------" & ^
 cd .. & ^
 set jdk_name=\OpenJDK8U-jdk_x64_linux_openj9_linuxXL_8u282b08_openj9-0.24.0.tar.gz
 bitsadmin.exe /transfer JobName https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_openj9_linuxXL_8u282b08_openj9-0.24.0.tar.gz %cd%\plink-docker\plink%jdk_name% & ^
-set flink_name=\flink-1.12.1-bin-scala_2.11.tgz
-bitsadmin.exe /transfer JobName https://mirrors.tuna.tsinghua.edu.cn/apache/flink/flink-1.12.1/flink-1.12.1-bin-scala_2.11.tgz %cd%\plink-docker\plink%flink_name% & ^
-
 mvn clean package -Dmaven.test.skip=true & ^
 copy /Y plink-dist\target\plink*.tar.gz plink-docker\plink & ^
-copy /Y plink-web\src\main\resources\META-INF\sql\mysql\plink_init.sql plink-docker\mysql & ^
-docker build -t hairless/plink:master plink-docker\plink & ^
-docker build -t plink/mysql-57-centos7 plink-docker\mysql & ^
+copy /Y config\plink_init.sql plink-docker\mysql & ^
+docker build -t plink/plink:master plink-docker\plink & ^
+docker build -t plink/mysql:latest plink-docker\mysql & ^
 cd plink-docker & ^
 echo "--------- build success ---------"
